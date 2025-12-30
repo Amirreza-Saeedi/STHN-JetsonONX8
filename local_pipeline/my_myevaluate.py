28a53226cbe01866da55cf1760763cd1ff23ed21
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2025-12-30 13:56:33 +0330

    js_runModels added

--- local_pipeline/my_myevaluate.py
+++ local_pipeline/my_myevaluate.py
@@ -112,11 +112,11 @@ def test(args, wandb_log):
     folder_name = "maps_results/farm"
     all_corners = []
     times = []
-    for i in range(270):
+    for i in range(1):
         try:
             # مسیر تصاویر با شماره i
-            img1_path = f"/content/drive/MyDrive/big_patches/big_picture_crop_{i}.tif"
-            img2_path = f"/content/drive/MyDrive/small_patches_thermal/small_picture_crop_{i}.tif"
+            img1_path = r"D:\Robotic Perceptoin Lab\map-matching\STHN-cpy\js_dataset\satellite\farm.tif"
+            img2_path = r"D:\Robotic Perceptoin Lab\map-matching\STHN-cpy\js_dataset\thermal\farm.tif"
     
             # خواندن تصاویر
             img1 = F.to_tensor(Image.open(img1_path).convert("RGB")).unsqueeze(0)
@@ -165,7 +165,7 @@ def test(args, wandb_log):
     # ذخیره در فایل Excel
     columns = ["image_index", "x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4"]
     df = pd.DataFrame(all_corners, columns=columns)
-    df.to_excel(f"/content/drive/MyDrive/predicted.xlsx", index=False)
+    df.to_excel(f"test/predicted.xlsx", index=False)
     print("📁 Saved all corner points to four_point_1_mul6.xlsx")
 
 
